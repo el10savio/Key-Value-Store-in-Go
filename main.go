945eb9cb73c517b380ad84e73d79ab2aae4f6d32
Elton SV <el10savio96@gmail.com>
2019-07-21 16:34:53 +0530

    Started again from scratch

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
-import (
-	database "./db"
-	kv "./kv"
-)
+import "os"
+
+type Entry struct {
+	Key   string
+	Value string
+}
 
 func main() {
 
-	db, err := database.InitDbFile()
+	file, err := os.OpenFile("kv.db", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	defer file.Close()
 
-	kv.PUT(kv.Entry{"1af75d", []byte("test-string")})
+	e1 := Entry{"key", "value"}
+	file.Write([]byte(e1.Key))
+	file.Write([]byte(":"))
+	file.Write([]byte(e1.Value))
+	file.Write([]byte("\n"))
 
-	kv.GET("1af75d")
+	e2 := Entry{"key2", "value2"}
+	file.Write([]byte(e2.Key))
+	file.Write([]byte(":"))
+	file.Write([]byte(e2.Value))
+	file.Write([]byte("\n"))
 
 }
